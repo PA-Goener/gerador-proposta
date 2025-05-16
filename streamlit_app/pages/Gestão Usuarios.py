15f37c7278bca965faa0546df6b9ae345d0e1218
P.A. <pa.sreboucas@gmail.com>
2025-05-16 17:19:47 -0300

    Tentado o deploy

--- streamlit_app/pages/Gestão Usuarios.py
+++ streamlit_app/pages/Gestão Usuarios.py
@@ -6,10 +6,10 @@ if not st.session_state.get('login_status', False) or not st.session_state.get('
     st.stop()
 from PIL import Image
 #Achando o caminho do icone
-icon_path = os.path.join('.streamlit','Logo_azul_quadrada.PNG')
+icon_path = os.path.join('streamlit_app/.streamlit','Logo_azul_quadrada.PNG')
 st.set_page_config(page_title="Gestão de Usuários", layout="wide",page_icon=icon_path)
 #Achando o caminho da logo
-img_path = os.path.join('.streamlit','Logo_goener_colorida.png')
+img_path = os.path.join('streamlit_app/.streamlit','Logo_goener_colorida.png')
 img = Image.open(img_path)
 # Redimensionar a imagem (alterando a altura e mantendo a proporção)
 img = img.resize((int(img.width * (50 / img.height)), 50))  # Altura = 30 pixels
